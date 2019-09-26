7251ae35735231759f2d1c8e768f4088bb4675c9
Sylvanus Kateile <kateile@outlook.com>
2019-09-26 23:07:59 +0300

    display election details

--- src/components/elections/electionView.js
+++ src/components/elections/electionView.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import { ProgressBar } from '../../layout/progressBar'
+import { Header } from 'semantic-ui-react'
+import ErrorMessage from '../../layout/errorMessage'
 
-export function ElectionView() {
-  return <div>
-    ElectionView
-  </div>
+const ELECTION_QUERY = gql`
+query($id: Int!) {
+  election(id: $id) {
+    title
+    isOpen
+    isStrict
+    startAt
+    endAt
+  }
+}
+`
+
+export function ElectionView({ match }) {
+  return <Query query={ELECTION_QUERY} variables={{ id: parseInt(match.params.id) }}>
+    {({ loading, data, error }) => {
+      if (loading) return <ProgressBar/>
+      if (error) return <ErrorMessage message={error.message}/>
+
+      const { title, isOpen, isStrict, startAt, endAt } = data.election
+
+      return (
+        <Header as='h1'>
+          {title}
+          <Header.Subheader>StartAt: {startAt}</Header.Subheader>
+          <Header.Subheader>EndAt: {endAt}</Header.Subheader>
+          <Header.Subheader>isOpen: {isOpen.toString()}</Header.Subheader>
+          <Header.Subheader>isStrict: {isStrict.toString()}</Header.Subheader>
+        </Header>
+      )
+    }}
+  </Query>
 }
--- src/components/elections/electionsList.js
+++ src/components/elections/electionsList.js
@@ -3,14 +3,14 @@ import { ElectionItem } from './electionItem'
 import { List } from 'semantic-ui-react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
+import { ProgressBar } from '../../layout/progressBar'
+import ErrorMessage from '../../layout/errorMessage'
 
 const ELECTIONS_QUERY = gql`
   {
     elections {
       id
       title
-      isOpen
-      isStrict
     }
   }
 `
@@ -18,8 +18,8 @@ const ELECTIONS_QUERY = gql`
 export const ElectionsList = () => (
   <Query query={ELECTIONS_QUERY}>
     {({ loading, data, error }) => {
-      if (loading) return <div>loading....</div>
-      if (error) return <div>{error.message}</div>
+      if (loading) return <ProgressBar/>
+      if (error) return <ErrorMessage message={error.message}/>
 
       return <List divided verticalAlign='middle' size="huge">
         {
