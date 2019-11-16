ec7e9aa0e1f7ac1b4e95ef056e052c7a21095c01
Sylvanus Kateile <kateile@outlook.com>
2019-11-16 10:07:17 +0300

    dispay warning message when no programs registered

--- src/components/program/programsList.js
+++ src/components/program/programsList.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { ProgramItem } from './programItem'
-import { List } from 'semantic-ui-react'
+import { List, Message } from 'semantic-ui-react'
 
-export const ProgramsList = ({ programs }) => (
-  <div>
-    <List divided verticalAlign='middle'>
-      {programs.map((p) => (
-        <ProgramItem program={p.program} />
-      ))}
-    </List>
-  </div>
-)
+export const ProgramsList = ({ programs }) => {
+  if (!programs) return <Message size='tiny' error>None registered, You need to register at least one program!</Message>
+  return (
+    <div>
+      <List divided verticalAlign='middle'>
+        {programs.map((p) => (
+          <ProgramItem program={p.program}/>
+        ))}
+      </List>
+    </div>
+  )
+}
